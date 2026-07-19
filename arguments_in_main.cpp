225335df051ab2dbb999f407268b283d957f5698
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-07-19 21:26:09 +0500

    apply changes

--- arguments_in_main.cpp
+++ arguments_in_main.cpp
@@ -1,10 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char **argv){
-    int disp, count;
+int main(int argc, char* argv[]){
+    int count;
     if(argc < 2){
-        cout << "enter the length of the count\n";
-        cout << "on the commad line "
+        cout << "Usage : program <count> [display]\n";
+        return 1;
+    }
+
+    count = atoi(argv[1]);
+    bool display = (argc == 3 && strcmp(argv[2], "display")==0);
+
+    while(count >0){
+        {
+            if(display)
+            cout << count << " "; 
+
+            count--;
+        }
+
+        cout << "\a";
+        return 0;
     }
 }
