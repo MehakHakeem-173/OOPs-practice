a7c7ee6cc5c2a2b5456a661761bd6ef94322c76b
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-07-28 17:37:05 +0500

    lecture 5-1

---
+++ CS201/3_5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>//console
+using namespace std;
+
+int main(){
+    
+    int amir_age = 21, amara_age = 19;
+
+    if(amir_age>amara_age){
+        cout << "amir is older than amara";
+    }
+
+    else{
+        cout << "amir is not older than amara";
+    }
+    return 0;
+}
